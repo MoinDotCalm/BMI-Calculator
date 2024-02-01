a69c8af5875f0a8cfa19b23a0ca1c10b0eff4003
MoinDotCalm <moin.ace1@gmail.com>
2024-02-01 15:25:37 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const BMICalculator(),
+    );
+  }
+}
+
+class BMICalculator extends StatefulWidget {
+  const BMICalculator({super.key});
+
+  @override
+  State<BMICalculator> createState() => _BMICalculatorState();
+}
+
+class _BMICalculatorState extends State<BMICalculator> {
+  TextEditingController heightController = TextEditingController();
+  TextEditingController weightController = TextEditingController();
+
+  double bmi = 0.0;
+
+  bool showBmi = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        
+        title: const Text('BMI Calculator'),
+      ),
+      body: Container(
+        padding:const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            TextField(
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Enter Height in cm',
+              ),
+              controller: heightController,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextField(
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Enter Weight in KG',
+              ),
+              controller: weightController,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  double height = double.parse(heightController.text);
+                  double weight = double.parse(weightController.text);
+                  bmi = (weight / (height * height)) * 10000;
+                  showBmi = true;
+                });
+              },
+              child: const Text(
+                'Calculate',
+                style:  TextStyle(
+                  fontSize: 30,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            showBmi
+                ? Text(
+                    "bmi: " + bmi.toStringAsFixed(2),
+                    style: const TextStyle(
+                      fontSize: 30,
+                    ),
+                  )
+                : const SizedBox(),
+          ],
+        ),
+     ),
+);
+}
+}
